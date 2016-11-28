89ebe06790e52829cedcd8fb86bdc2b91515c6b2
Madeline Nowicki <madnowicki@gmail.com>
2016-11-28 18:32:47 -0500

    for pts

--- mygame.py
+++ mygame.py
@@ -3,7 +3,13 @@ from pygame.sprite import *
 from random import *
 
 DELAY = 1000;
-bgcolor = (0, 42, 196)
+
+class Background(pygame.sprite.Sprite):
+    def __init__(self, image_file, location):
+        pygame.sprite.Sprite.__init__(self)  #call Sprite initializer
+        self.image = pygame.image.load("flag.bmp")
+        self.rect = self.image.get_rect()
+        self.rect.left, self.rect.top = location
 
 class Almond(Sprite):
     def __init__(self):

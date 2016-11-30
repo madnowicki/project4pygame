7e268c40b297a62eaf70c4caca7d8a9317c3b7a6
Madeline Nowicki <madnowicki@gmail.com>
2016-11-30 15:56:41 -0500

    intro and bg and sound

--- mygame.py
+++ mygame.py
@@ -1,15 +1,15 @@
 from pygame import *
 from pygame.sprite import *
 from random import *
-#comment
-DELAY = 1000;
 
-class Background(pygame.sprite.Sprite):
-    def __init__(self, image_file, location):
-        pygame.sprite.Sprite.__init__(self)  #call Sprite initializer
-        self.image = pygame.image.load("flag.bmp")
-        self.rect = self.image.get_rect()
-        self.rect.left, self.rect.top = location
+DELAY = 1000;
+maxHeight = 600
+maxWidth = 800
+screen = display.set_mode((maxWidth, maxHeight))
+display.set_caption('The Snack-uation Room')
+bgimage = pygame.image.load("flag.bmp").convert_alpha()
+bgimage = pygame.transform.scale(bgimage, (maxWidth, maxHeight))
+screen.blit(bgimage, (0,0))
 
 class Almond(Sprite):
     def __init__(self):
@@ -33,14 +33,56 @@ class Obama(Sprite):
 
     def update(self):
         self.rect.center = mouse.get_pos()
+'''
+class Star(Sprite):
+    def __init__(self, x, y):
+        super(Star, self).__init__()
+        self.image = pygame.Surface((2, 2))
+        pygame.draw.circle(self.image,
+                           (128, 128, 200),
+                           (0, 0),
+                           2,
+                           0)
+        self.rect = self.image.get_rect()
+        self.rect.center = (x, y)
+        self.velocity = 1
+        self.size = 1
+        self.colour = 128
 
+    def accelerate(self):
+        self.image = pygame.Surface((1, self.size))
+
+        if self.size < 200:
+            self.size += 4
+            self.colour += 20
+            if self.colour >= 200:
+                self.colour = random.randint(180, 200)
+        else:
+            self.colour -= 30
+            if self.colour <= 20:
+                self.colour = random.randrange(20)
+
+        pygame.draw.line(self.image, (self.colour, self.colour, self.colour),
+                         (0, 0), (0, self.size))
+
+        if self.velocity < Y_MAX / 3:
+            self.velocity += 1
+
+        # x, y = self.rect.center
+        # self.rect.center = random.randrange(X_MAX), y
+        def update(self):
+        x, y = self.rect.center
+        if self.rect.center[1] > Y_MAX:
+            self.rect.center = (x, 0)
+        else:
+            self.rect.center = (x, y + self.velocity)
+'''
 init()
 
-maxHeight = 800
-maxWidth = 800
-screen = display.set_mode((maxWidth, maxHeight))
-display.set_caption('The Snack-uation Room')
-bgcolor = image.load("flag.bmp").convert_alpha()
+
+
+#stars = create_starfield(everything)
+
 
 mouse.set_visible(False)
 
@@ -54,26 +96,18 @@ snacks = 0
 time.set_timer(USEREVENT + 1, DELAY)
 
 while True:
-    e = event.poll()
-    if e.type == QUIT:
-        quit()
-        break
+    for event in event.get():
+        if event.type == QUIT
+        done = True
 
-    elif e.type == MOUSEBUTTONDOWN:
-        if obama.eat(almond):
-            mixer.Sound("cha-ching.wav").play()
-            almond.move()
-            hits += 1
-
-            time.set_timer(USEREVENT + 1, DELAY)
-
-    elif e.type == USEREVENT + 1:
-        almond.move()
+    pos = mouse.get_pos()
+    
         
 #how to upload background image??
-    screen.image.load("flag.bmp").convert_alpha()
-    t = f.render("Jackpot = " + str(snacks), False, (0,0,0))
+    #screen.image.load("flag.bmp").convert_alpha()
+    t = f.render("Snackpot = " + str(snacks), False, (0,0,0))
     screen.blit(t, (320,0))
+    screen.blit(bgimage, (0,0))
 
     sprites.update()
     sprites.draw(screen)
---
+++ mygame2.py
@@ -0,0 +1,98 @@
+from pygame import *
+from pygame.sprite import *
+from random import *
+init()
+
+#DELAY = 1000;
+maxHeight = 600
+maxWidth = 800
+screen = display.set_mode((maxWidth, maxHeight))
+display.set_caption('The Snack-uation Room')
+
+class Almond(Sprite):
+    def __init__(self):
+        Sprite.__init__(self)
+        self.image = image.load("almond.bmp").convert_alpha()
+        self.rect = self.image.get_rect()
+
+    def move(self):
+        randX = randint(0, 800)
+        randY = randint(0, 800)
+        self.rect.center = (randX, randY)
+
+class Obama(Sprite):
+    def __init__(self):
+        Sprite.__init__(self)
+        self.image = image.load("obama.bmp").convert()
+        self.rect = self.image.get_rect()
+
+    def eat(self, target):
+        return self.rect.colliderect(target)
+
+    def update(self):
+        self.rect.center = mouse.get_pos()
+
+def intro():
+	bgimage = pygame.image.load("const.bmp").convert_alpha()
+	bgimage = pygame.transform.scale(bgimage, (maxWidth, maxHeight))
+	screen.blit(bgimage, (0,0))
+	f = font.Font(None, 30)
+	t = f.render("Intro screen and instructions", False, (0,0,0))
+	screen.blit(t, (320,0))	
+
+	while True:
+		for e in event.get():
+			if e.type == KEYDOWN:
+				if e.key == K_SPACE:
+					return
+		screen.blit(bgimage, (0,0))
+		screen.blit(t, (320,0))
+		display.flip()
+	pygame.quit()
+
+intro()
+def main():
+	bgimage = pygame.image.load("flag.bmp").convert()
+	bgimage = pygame.transform.scale(bgimage, (maxWidth, maxHeight))
+	screen.blit(bgimage, (0,0))
+
+
+	mouse.set_visible(False)
+	f = font.Font(None, 65)
+
+	almond = Almond()
+	obama = Obama()
+	sprites = RenderPlain(almond, obama)
+
+	snacks = 0
+
+	while True:
+	    e = event.poll()
+	    if e.type == QUIT:
+	        quit()
+	        break
+
+	    elif e.type == MOUSEBUTTONDOWN:
+	        if obama.eat(almond):
+	            mixer.Sound("bite.wav").play()
+	            almond.move()
+	            snacks += 1
+
+	            # reset timer
+	            
+	    elif e.type == USEREVENT + 1: # TIME has passed
+	        almond.move()
+
+	    # refill background color so that we can paint sprites in new locations
+	    #screen.fill(bgcolor)
+	    screen.blit(bgimage, (0,0))
+	    t = f.render("Snackpot = " + str(snacks), False, (0,0,0))
+	    screen.blit(t, (320, 0))        # draw text to screen.  Can you move it?
+
+	    # update and redraw sprites
+	    sprites.update()
+	    sprites.draw(screen)
+	    display.update()
+	#snacks = 0
+
+main()
